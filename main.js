5faf507e1093f89f93affa9cdbe8b1625b3d131e
Zeldariomon <puniayash@gmail.com>
2022-01-12 16:35:10 +0530

    added 2 photos

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './style.css'
 
+const nextButton = document.querySelector(".btn");
+const skybox = document.querySelector('a-sky');
+
+const pictures = [
+  '#church',
+  '#fort'
+];
+
+let index = 0;
+
+nextButton.addEventListener('click', function (event) {
+  skybox.setAttribute("src", pictures[(++index) % pictures.length]);
+});
